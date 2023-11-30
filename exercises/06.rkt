4b30bb8abe91f95954f6dc79c4c6c1e8ba038e06
Rok Nikolič <rok.nikolic@gmail.com>
2023-11-30 14:55:57 +0100

    06 done

--- exercises/06.rkt
+++ exercises/06.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(provide (all-defined-out))
+
 (define (power x n)
     (if (= n 0)
         1
@@ -20,3 +22,41 @@
         [(= x 2) 1]
         [#t (fib_helper 1 1 3)]))      
 
+(define (reverse list_in)
+    (if (null? list_in)
+        null
+        (append (reverse (cdr list_in)) (list (car list_in)))))
+
+(define (remove x list_in)
+    (cond 
+        [(null? list_in) null]
+        [(eq? x (car list_in)) (append null (remove x (cdr list_in)))]
+        [#t (append (list (car list_in)) (remove x (cdr list_in)))]))
+
+(define (map f list_in)
+    (cond 
+        [(null? list_in) null]
+        [#t (append (list (f (car list_in))) (map f (cdr list_in)))]))
+
+(define (filter f list_in)
+    (cond 
+        [(null? list_in) null]
+        [(f (car list_in)) (append (list (car list_in)) (filter f (cdr list_in)))]
+        [#t (append null (filter f (cdr list_in)))]))
+
+(define (zip list1 list2)
+    (cond 
+        [(or (null? list1) (null? list2)) null]
+        [#t (append (list (cons (car list1) (car list2))) (zip (cdr list1) (cdr list2)))]))
+
+(define (range start stop step)
+    (cond 
+    [(> start stop) null]
+    [#t (append (list start) (range (+ start step) stop step))]))
+
+(define (is-palindrome list_in)
+    (define (reverse list_r)
+        (cond 
+            [(null? list_r) null]
+            [#t (append (reverse (cdr list_r)) (list (car list_r)))]))
+    (equal? list_in (reverse list_in)))
