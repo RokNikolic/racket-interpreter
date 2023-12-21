8ca21b383d6eb573d1f8fe0bdcadcfbd10a41a1f
Rok Nikolič <rok.nikolic@gmail.com>
2023-12-21 19:03:42 +0100

    08 done, needes polish and testing

--- exercises/08.rkt
+++ exercises/08.rkt
@@ -5,10 +5,12 @@
 (struct int (input) #:transparent)
 (struct true () #:transparent)
 (struct false () #:transparent)
-(struct add (val1 val2) #:transparent)
-(struct mul (val1 val2) #:transparent)
+(struct add (input1 input2) #:transparent)
+(struct mul (input1 input2) #:transparent)
+(struct ?leq (input1 input2) #:transparent)
 (struct ~ (input) #:transparent)
 (struct ?int (input) #:transparent)
+(struct if-then-else (condition input1 input2) #:transparent)
 
 (define (fri expression)
     (cond
@@ -17,8 +19,8 @@
         [(false? expression) (false)]
         [(add? expression) 
             (let (
-                [val1 (fri (add-val1 expression))]
-                [val2 (fri (add-val2 expression))])
+                [val1 (fri (add-input1 expression))]
+                [val2 (fri (add-input2 expression))])
             (cond
                 [(and (int? val1) (int? val2))
                     (int (+ (int-input val1) (int-input val2)))]
@@ -26,12 +28,12 @@
                     (true)]
                 [(and (false? val1) (false? val2))
                     (false)]
-                [#t (error "Value not an int or bool!")])
+                [#t (error "Error, value not an int or bool.")])
         )]
         [(mul? expression) 
             (let (
-                [val1 (fri (mul-val1 expression))]
-                [val2 (fri (mul-val2 expression))])
+                [val1 (fri (mul-input1 expression))]
+                [val2 (fri (mul-input2 expression))])
             (cond
                 [(and (int? val1) (int? val2))
                     (int (* (int-input val1) (int-input val2)))]
@@ -39,7 +41,22 @@
                     (true)]
                 [(or (false? val1) (false? val2))
                     (false)]
-                [#t (error "Value not an int or bool!")])
+                [#t (error "Error, value not an int or bool.")])
+        )]
+        [(?leq? expression) 
+            (let (
+                [val1 (fri (?leq-input1 expression))]
+                [val2 (fri (?leq-input2 expression))])
+            (cond
+                [(and (int? val1) (int? val2))
+                    (if (<= (int-input val1) (int-input val2))
+                        (true)
+                        (false))]
+                [(or (true? val1) (false? val2))
+                    (false)]
+                [(or (false? val1) (true? val2))
+                    (true)]
+                [#t (error "Error, value not an int or bool.")])
         )]
         [(~? expression) 
             (let ([val_input (fri (~-input expression))])
@@ -47,7 +64,7 @@
                 [(int? val_input) (int (- (int-input val_input)))]
                 [(true? val_input) (false)]
                 [(false? val_input) (true)]
-                [#t (error "Cant negate this type of thing")])
+                [#t (error "Error, can't negate this type of thing.")])
         )]
         [(?int? expression) 
             (let ([expression_input (fri (?int-input expression))])
@@ -55,37 +72,20 @@
                 (true)
                 (false))
         )]
+        [(if-then-else? expression) 
+            (let ([condition_input (fri (if-then-else-condition expression))])
+            (cond 
+                [(true? condition_input) (fri (if-then-else-input1 expression))]
+                [(false? condition_input) (fri (if-then-else-input2 expression))]
+                [#t (error "Error, can't evaluate condition.")])
+        )]
+        [#t (error "Error, wrong syntax.")]
     ))
 
+(define (conditional condition_input input . other)
+    (if  (equal? 1 (length other)) 
+        (if-then-else condition_input input (car other))
+        (if-then-else condition_input input (apply conditional other))))
 
-(fri (?int (false)))
-
-(struct konst (int) #:transparent)     ; konstanta; argument je število
-(struct bool (b) #:transparent)        ; b ima lahko vrednost true or false
-(struct negiraj (e) #:transparent)     ; e je lahko izraz
-(struct sestej (e1 e2) #:transparent)  ; e1 in e2 sta izraza
-(struct ce-potem-sicer (pogoj res nires) #:transparent) ; pogoj, res, nires hranijo izraze
-
-(define (jais e)
-  (cond [(konst? e) e]   ; vrnemo izraz v ciljnem jeziku
-        [(bool? e) e]
-        [(negiraj? e) 
-         (let ([v (jais (negiraj-e e))])
-           (cond [(konst? v) (konst (- (konst-int v)))]
-                 [(bool? v) (bool (not (bool-b v)))]
-                 [#t (error "negacija nepričakovanega izraza")]))]
-        [(sestej? e) 
-         (let ([v1 (jais (sestej-e1 e))]
-               [v2 (jais (sestej-e2 e))])
-           (if (and (konst? v1) (konst? v2))
-               (konst (+ (konst-int v1) (konst-int v2)))
-               (error "seštevanec ni številka")))]
-        [(ce-potem-sicer? e) 
-         (let ([v-test (jais (ce-potem-sicer-pogoj e))])
-           (if (bool? v-test)
-               (if (bool-b v-test)
-                   (jais (ce-potem-sicer-res e))
-                   (jais (ce-potem-sicer-nires e)))
-               (error "pogoj ni logična vrednost")))]
-        [#t (error "sintaksa izraza ni pravilna")]
-        ))
+(define (?geq expresion1 expression2)
+    (?leq expression2 expresion1))
