b6eb5d54a3628852f749953a345aee693bb40de4
Rok Nikolič <rok.nikolic@gmail.com>
2023-12-24 17:31:26 +0100

    any and all done

--- project/02-project.rkt
+++ project/02-project.rkt
@@ -51,9 +51,9 @@
 (define (fri expression environment)
     (cond
         ; Data types
-        [(int? expression) expression]
         [(true? expression) expression]
         [(false? expression) expression]
+        [(int? expression) expression]
         [(empty? expression) expression]
         [(exception? expression) 
             (if (string? (exception-exn expression))
@@ -97,55 +97,8 @@
                 [(triggered? condition_input) condition_input]
                 [(true? condition_input) (fri (if-then-else-e1 expression) environment)]
                 [(false? condition_input) (fri (if-then-else-e2 expression) environment)]
+                [(int? condition_input) (fri (if-then-else-e1 expression) environment)]
                 [#t (triggered (exception "if-then-else: wrong argument type"))])
-        )]
-        [(add? expression) 
-            (let (
-                [val1 (fri (add-e1 expression) environment)]
-                [val2 (fri (add-e2 expression) environment)])
-            (cond
-                [(triggered? val1) val1]
-                [(triggered? val2) val2]
-                [(and (int? val1) (int? val2))
-                    (int (+ (int-n val1) (int-n val2)))]
-                [(or (int? val1) (int? val2))
-                    (triggered (exception "add: wrong argument type"))]
-                [(empty? val1) val2]
-                [(empty? val2) val1]
-                [(and (..? val1) (..? val2))
-                    (let (
-                        [head_of_1 (..-e1 val1)]
-                        [tail_of_1 (..-e2 val1)])
-                    (cond
-                        [(empty? tail_of_1) (.. head_of_1 val2)]
-                        [(..? tail_of_1) (.. head_of_1 (fri (add tail_of_1 val2) environment))]
-                        [#t (triggered (exception "add: wrong argument type"))]
-                    )
-                )]
-                [(or (..? val1) (..? val2))
-                    (triggered (exception "add: wrong argument type"))]
-                [(or (true? val1) (true? val2))
-                    (true)]
-                [(and (false? val1) (false? val2))
-                    (false)]
-                [#t (triggered (exception "add: wrong argument type"))]))
-        ]
-        [(mul? expression) 
-            (let (
-                [val1 (fri (mul-e1 expression) environment)]
-                [val2 (fri (mul-e2 expression) environment)])
-            (cond
-                [(triggered? val1) val1]
-                [(triggered? val2) val2]
-                [(and (int? val1) (int? val2))
-                    (int (* (int-n val1) (int-n val2)))]
-                [(or (int? val1) (int? val2))
-                    (triggered (exception "mul: wrong argument type"))]
-                [(and (true? val1) (true? val2))
-                    (true)]
-                [(or (false? val1) (false? val2))
-                    (false)]
-                [#t (triggered (exception "mul: wrong argument type"))])
         )]
         [(?int? expression) 
             (let ([expression_input (fri (?int-e expression) environment)])
@@ -193,6 +146,54 @@
                 [(exception? expression_input) (true)]
                 [#t (false)])
         )]
+        [(add? expression) 
+            (let (
+                [val1 (fri (add-e1 expression) environment)]
+                [val2 (fri (add-e2 expression) environment)])
+            (cond
+                [(triggered? val1) val1]
+                [(triggered? val2) val2]
+                [(and (int? val1) (int? val2))
+                    (int (+ (int-n val1) (int-n val2)))]
+                [(or (int? val1) (int? val2))
+                    (triggered (exception "add: wrong argument type"))]
+                [(empty? val1) val2]
+                [(empty? val2) val1]
+                [(and (..? val1) (..? val2))
+                    (let (
+                        [head_of_1 (..-e1 val1)]
+                        [tail_of_1 (..-e2 val1)])
+                    (cond
+                        [(empty? tail_of_1) (.. head_of_1 val2)]
+                        [(..? tail_of_1) (.. head_of_1 (fri (add tail_of_1 val2) environment))]
+                        [#t (triggered (exception "add: wrong argument type"))]
+                    )
+                )]
+                [(or (..? val1) (..? val2))
+                    (triggered (exception "add: wrong argument type"))]
+                [(or (true? val1) (true? val2))
+                    (true)]
+                [(and (false? val1) (false? val2))
+                    (false)]
+                [#t (triggered (exception "add: wrong argument type"))])
+        )]
+        [(mul? expression) 
+            (let (
+                [val1 (fri (mul-e1 expression) environment)]
+                [val2 (fri (mul-e2 expression) environment)])
+            (cond
+                [(triggered? val1) val1]
+                [(triggered? val2) val2]
+                [(and (int? val1) (int? val2))
+                    (int (* (int-n val1) (int-n val2)))]
+                [(or (int? val1) (int? val2))
+                    (triggered (exception "mul: wrong argument type"))]
+                [(and (true? val1) (true? val2))
+                    (true)]
+                [(or (false? val1) (false? val2))
+                    (false)]
+                [#t (triggered (exception "mul: wrong argument type"))])
+        )]
         [(?leq? expression) 
             (let (
                 [val1 (fri (?leq-e1 expression) environment)]
@@ -206,6 +207,16 @@
                         (false))]
                 [(or (int? val1) (int? val2))
                     (triggered (exception "?leq: wrong argument type"))]
+                [(empty? val1) (true)]
+                [(empty? val2) (false)]
+                [(and (..? val1) (..? val2))
+                    (let (
+                        [tail_of_1 (..-e2 val1)]
+                        [tail_of_2 (..-e2 val2)])
+                    (fri (?leq tail_of_1 tail_of_2) environment)
+                )]
+                [(or (..? val1) (..? val2))
+                    (triggered (exception "add: wrong argument type"))]
                 [(and (true? val1) (false? val2))
                     (false)]
                 [(or (false? val1) (true? val2))
@@ -214,8 +225,8 @@
         )]
         [(?=? expression) 
             (let (
-                [expression1 (fri (?leq-e1 expression) environment)]
-                [expression2 (fri (?leq-e2 expression) environment)])
+                [expression1 (fri (?=-e1 expression) environment)]
+                [expression2 (fri (?=-e2 expression) environment)])
             (cond
                 [(triggered? expression1) expression1]
                 [(triggered? expression2) expression2]
@@ -241,17 +252,50 @@
                 [#t (triggered (exception "tail: wrong argument type"))])
         )]
         [(~? expression) 
-            (let ([val_input (fri (~-e expression) environment)])
+            (let ([expression_input (fri (~-e expression) environment)])
             (cond 
-                [(triggered? val_input) val_input]
-                [(int? val_input) (int (- (int-n val_input)))]
-                [(true? val_input) (false)]
-                [(false? val_input) (true)]
+                [(triggered? expression_input) expression_input]
+                [(int? expression_input) (int (- (int-n expression_input)))]
+                [(true? expression_input) (false)]
+                [(false? expression_input) (true)]
                 [#t (triggered (exception "~: wrong argument type"))])
         )]
-        [(?all? expression) expression] ;TODO
-
-        [(?any? expression) expression] ;TODO
+        [(?all? expression) 
+            (letrec (
+                [expression_input (fri (?all-e expression) environment)]
+                [seq_flag (fri (?seq expression_input) environment)])
+            (cond
+                [(triggered? expression_input) expression_input]
+                [(false? seq_flag) (triggered (exception "?all: wrong argument type"))]
+                [(empty? expression_input) (true)]
+                [(..? expression_input)
+                    (let (
+                        [head_of_1 (..-e1 expression_input)]
+                        [tail_of_1 (..-e2 expression_input)])
+                    (if (false? head_of_1)
+                        (false)
+                        (fri (?all tail_of_1) environment))
+                )]
+                [#t (triggered (exception "?all: wrong argument type"))])
+        )]
+        [(?any? expression) 
+            (letrec (
+                [expression_input (fri (?any-e expression) environment)]
+                [seq_flag (fri (?seq expression_input) environment)])
+            (cond
+                [(triggered? expression_input) expression_input]
+                [(false? seq_flag) (triggered (exception "?any: wrong argument type"))]
+                [(empty? expression_input) (false)]
+                [(..? expression_input)
+                    (let (
+                        [head_of_1 (..-e1 expression_input)]
+                        [tail_of_1 (..-e2 expression_input)])
+                    (if (true? head_of_1)
+                        (true)
+                        (fri (?all tail_of_1) environment))
+                )]
+                [#t (triggered (exception "?any: wrong argument type"))])
+        )]
 
         ; Variables
         [(vars? expression) expression] ;TODO
@@ -270,6 +314,7 @@
         [#t (triggered (exception "fri: wrong syntax."))]
     ))
 
+; Macros
 (define (greater e1 e2)
     (fri (int 1) null))
 
